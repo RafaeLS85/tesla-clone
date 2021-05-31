2539942a02cc629ad2b01234a97625467ffc8894
RafaeLS85 <rafaelsebastian@live.com.ar>
2021-05-31 20:04:11 -0300

    add redux store

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import MenuIcon from "@material-ui/icons/Menu";
 import CloseIcon from '@material-ui/icons/Close';
+import { selectCars } from '../features/car/carSlice'
+import { useSelector } from 'react-redux'
 
 function Header() {
 
     const [burgerStatus, setBurgerStatus] = useState(false)
+
+    const cars = useSelector(selectCars)   
+
 
   return (
     <Container>
@@ -13,10 +18,10 @@ function Header() {
         <img src="/images/logo.svg" />
       </a>
       <Menu>
-        <a href="#">Model S</a>
-        <a href="#">Model 3</a>
-        <a href="#">Model X</a>
-        <a href="#">Model Y</a>
+          {/*Usando la data del store para mostrar el menu  */}
+        {cars && cars.map((car, index)=>(
+            <a key={index} href="#">{car}</a>
+        ))}        
       </Menu>
       <RightMenu>
         <a href="#">Shop</a>
@@ -26,16 +31,17 @@ function Header() {
       <BurgerNav show={burgerStatus}>
           <CloseWrapper>
             <CustomClose onClick={()=>setBurgerStatus(false)}/>
-          </CloseWrapper>         
-          <li><a href="">Existing Inventory</a></li>
-          <li><a href="">Use Inventory</a></li>
-          <li><a href="">Trade-In</a></li>
-          <li><a href="">Cybertruck</a></li>
-          <li><a href="">Roadster</a></li>
-          <li><a href="">Semi</a></li>
-          <li><a href="">Solar Roof</a></li>
-          <li><a href="">Solar Panels</a></li>
-          <li><a href="">More</a></li>
+          </CloseWrapper>  
+            {/*Usando la data del store para mostrar el menu  */}
+          {cars && cars.map((car, index)=>(
+              <li key={index}><a  href="#">{car}</a></li>            
+          ))}    
+
+           <li><a href="#">Existing Inventory</a></li>       
+           <li><a href="#">Used Inventory</a></li>       
+           <li><a href="#">Trade-in</a></li>       
+           <li><a href="#">Cybertruck</a></li>       
+           <li><a href="#">Roadster</a></li>       
 
       </BurgerNav>
     </Container>
